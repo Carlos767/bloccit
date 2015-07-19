a2f89f844e9c05cf0afeb90ba6cefe41bfc7dd98
Carlos767 <cjparrillasf@gmail.com>
2015-07-19 14:25:43 -0700

    AssociationsCheckpoint

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,7 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 
 	default_scope {order('created_at DESC')}
+	scope :ordered_by_title, -> (title) { where ("ordered_by <?", title)}
+	scope :ordered_by_reverse_created_at, -> (reverse) {where ("created_at <?", reverse)}
 
 end
