39ad9cf49d9354842d6d1cd7018a271cd4e0dc0a
Carlos767 <cjparrillasf@gmail.com>
2015-07-26 22:57:08 -0700

    adding more topics and posting paginations

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,7 @@
 class TopicsController < ApplicationController
   def index
-  	@topics = Topic.paginate(page: params[:page], per_page: 10)
+  	@topics = Topic.paginate(page: params[:page])
+    WillPaginate.per_page = 50
   	authorize @topics
   end
 
@@ -11,7 +12,8 @@ class TopicsController < ApplicationController
 
   def show
   	@topic = Topic.find(params[:id])
-  	@posts = @topic.posts.paginate(page: params[:page], per_page: 10)
+  	@posts = @topic.posts.paginate(page: params[:page])
+    WillPaginate.per_page = 100
   end
 
   def edit
