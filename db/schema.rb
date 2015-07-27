28a1cba9102d0023b1f53231226004177c89ade3
Carlos767 <cjparrillasf@gmail.com>
2015-07-26 21:52:10 -0700

    Complete uploading images assignment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724153021) do
+ActiveRecord::Schema.define(version: 20150727042608) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20150724153021) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
