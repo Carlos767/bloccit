1237c20e0219c8a294f2d4cd1a615fe57a192e69
Carlos767 <cjparrillasf@gmail.com>
2015-08-11 08:54:57 -0700

    making public profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  
+
   get 'summaries/index'
 
   get 'summaries/new'
@@ -11,6 +13,7 @@ Rails.application.routes.draw do
   resources :users, only: [:update]
   resources :questions
   resources :advertisements
+  resources :show
 
   resources :topics do
   	resources :posts, except: [:index] do
